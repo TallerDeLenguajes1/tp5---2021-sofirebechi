5dd5d6cf75ba678433b5add5aeee3acb79f124df
Sofia <sofia.rebechi@gmail.com>
2021-06-17 20:31:01 -0300

    ultimos detalles- finalizado

--- Ejercicio2/Ejercicio2/Program.cs
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -6,70 +6,101 @@ namespace Ejercicio2
     {
         public static void Main(string[] args)
         {
-            int opcion;
+            int opcion = 1;
             float num1, num2, aux;
 
-            Console.WriteLine("* * * * CALCULADORA * * * *");
-            Console.WriteLine("Seleccione la opcion deseada \n");
-            Console.WriteLine("1-SUMA \n");
-            Console.WriteLine("2-RESTA \n");
-            Console.WriteLine("3-MULTIPLICACION \n");
-            Console.WriteLine("4-DIVIDIR \n");
+         
 
-            opcion = Convert.ToInt32(Console.ReadLine());
+             while (opcion == 1)
+             {
+                Console.WriteLine("* * * * CALCULADORA * * * *");
+                Console.WriteLine("Seleccione la opcion deseada \n");
+                Console.WriteLine("1-SUMA \n");
+                Console.WriteLine("2-RESTA \n");
+                Console.WriteLine("3-MULTIPLICACION \n");
+                Console.WriteLine("4-DIVIDIR \n");
 
+                opcion = Convert.ToInt32(Console.ReadLine());
 
-            if (opcion <= 4)
-            {
-
-            Console.WriteLine("Ingrese el primer numero:");
-
-
-            num1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Ingrese el segundo numero:");
-
-
-            num2 = Convert.ToInt32(Console.ReadLine());
-                switch (opcion)
+                if (opcion > 4)
                 {
-
-
-                    case 1:
-
-                        aux = num1 + num2;
-                        Console.WriteLine("El resultado de la operacion es : " + aux);
-                        break;
-
-
-                    case 2:
-
-                        aux = num1 - num2;
-                        Console.WriteLine("El resultado de la operacion es : " + aux);
-                        break;
-
-                    case 3:
-
-                        aux = num1 * num2;
-                        Console.WriteLine("El resultado de la operacion es : " + aux);
-                        break;
-                    case 4:
-
-                        aux = num1 / num2;
-                        Console.WriteLine("El resultado de la operacion es : " + aux);
-                        break;
+                    Console.WriteLine("## ERROR ## OPCION INVALIDA!");
+                    Console.WriteLine("¿Desea realizar otra operaciòn? /n");
+                    Console.WriteLine("1- SI /n");
+                    Console.WriteLine("2- NO /n");
+                    opcion = Convert.ToInt32(Console.ReadLine());
 
                 }
-            }
-            else{
+                else
+                {
+                    Console.WriteLine("Ingrese el primer numero:");
 
-                Console.WriteLine("## ERROR ## OPCION INVALIDA!");
-            }
+
+                    num1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Ingrese el segundo numero:");
+
+
+                    num2 = Convert.ToInt32(Console.ReadLine());
+
+                    switch (opcion)
+                    {
+
+
+                        case 1:
+
+                            aux = num1 + num2;
+                            Console.WriteLine("El resultado de la operacion es : " + aux);
+                            Console.WriteLine("¿Desea realizar otra operaciòn? /n");
+                            Console.WriteLine("1- SI /n");
+                            Console.WriteLine("2- NO /n");
+
+                            opcion = Convert.ToInt32(Console.ReadLine());
+                            break;
+
+
+                        case 2:
+
+                            aux = num1 - num2;
+                            Console.WriteLine("El resultado de la operacion es : " + aux);
+                            Console.WriteLine("¿Desea realizar otra operaciòn? /n");
+                            Console.WriteLine("1- SI /n");
+                            Console.WriteLine("2- NO /n");
+
+                            opcion = Convert.ToInt32(Console.ReadLine());
+                            break;
+
+                        case 3:
+
+                            aux = num1 * num2;
+                            Console.WriteLine("El resultado de la operacion es : " + aux);
+                            Console.WriteLine("¿Desea realizar otra operaciòn? /n");
+                            Console.WriteLine("1- SI /n");
+                            Console.WriteLine("2- NO /n");
+
+                            opcion = Convert.ToInt32(Console.ReadLine());
+                            break;
+
+                        case 4:
+
+                            aux = num1 / num2;
+                            Console.WriteLine("El resultado de la operacion es : " + aux);
+                            Console.WriteLine("¿Desea realizar otra operaciòn? /n");
+                            Console.WriteLine("1- SI /n");
+                            Console.WriteLine("2- NO /n");
+
+                            opcion = Convert.ToInt32(Console.ReadLine());
+                            break;
+
+                    }
+                }
+                }
+
+        }
 
         }
     }
 
-}
 
     
 
